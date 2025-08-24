60c2d72d550772081251212a86517cfe6f772763
张林锋 <657966747@qq.com>
2025-08-24 16:11:22 +0800

    1.完善安全认证服务

---
+++ biscuit-auth/biscuit-auth-server/src/main/java/cn/bbwres/biscuit/module/auth/config/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+/*
+ *
+ *  * Copyright 2024 bbwres
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package cn.bbwres.biscuit.module.auth.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+/**
+ * 加载用户
+ * @author zhanglinfeng
+ */
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    /**
+     * 加载用户
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
