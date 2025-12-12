df3ddb93410c9101fbc0bc59f3fdfccb0d6d35b8
张林锋 <657966747@qq.com>
2025-12-12 17:54:14 +0800

    1.增加监控服务

--- biscuit-gateway/src/main/java/cn/bbwres/biscuit/module/gateway/GatewayApplication.java
+++ biscuit-gateway/src/main/java/cn/bbwres/biscuit/module/gateway/GatewayApplication.java
@@ -21,6 +21,7 @@ package cn.bbwres.biscuit.module.gateway;
 import cn.bbwres.biscuit.BootstrapProfile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 /**
  * 网关服务
@@ -29,8 +30,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class GatewayApplication {
+
+
     public static void main(String[] args) {
         BootstrapProfile.setBootstrapProfile();
+        //作用是启用「自动上下文传播」机制—— 让线程上下文（如 MDC 日志上下文、事务上下文、安全上下文、请求头上下文等）在异步线程、虚拟线程、线程池任务、CompletableFuture 等场景下，
+        // 自动从父线程传递到子线程 / 异步任务中，无需手动传递上下文
+        Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
